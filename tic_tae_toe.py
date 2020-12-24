52877cacd2b02e1b74661637c9f1c254a3c13267
darshitpurohit101 <purohitdarshit101@gmail.com>
2020-12-24 17:26:00 +0530

    grid game

--- tic_tae_toe.py
+++ tic_tae_toe.py
@@ -1,108 +1,47 @@
-import numpy as np 
-import random 
-from time import sleep 
+import numpy as np
+import random
 
-# Creates an empty board 
-def create_board(): 
-	return(np.array([[0, 0, 0], 
-					[0, 0, 0], 
-					[0, 0, 0]])) 
+def check_board(board):
+    empty = []
+    
+    for row in range(len(board)):
+        for box in range(len(board[row])):
+            if board[row][box] == 0:
+                empty.append([row,box])
+    if len(empty) == 0:
+        return 0
+    else:
+        return empty
 
-# Check for empty places on board 
-def possibilities(board): 
-	l = [] 
-	
-	for i in range(len(board)): 
-		for j in range(len(board)): 
-			
-			if board[i][j] == 0: 
-				l.append((i, j)) 
-	return(l) 
+def state_reg(board):
+    current_state = []
+    for x in board:
+        for j in range(len(x)):
+            current_state.append(x[j])
+    return current_state
+    
+def play(board):
+    state = []
+    #Player one
+    x,y = map(int,input("Human's turn! ").split(' '))
+    board[(x,y)] = 1
+    
+    c_s = state_reg(board)
+    state.append(c_s)
 
-# Select a random place for the player 
-def random_place(board, player):
-#    print(selection)
-    x,y = map(int, input('enter').split(' '))
-    board[(x,y)] = player
-    selection = possibilities(board)
-    current_loc = random.choice(selection)
-    player += 1
-    board[current_loc] = player
-    return(board) 
+    possible_play = check_board(board)
+    if possible_play == 0:
+        print(state)
+        return 0
+    #player two
+    selection = random.choice(possible_play)
+    board[selection[0]][selection[1]] = 2
+    
+    c_s = state_reg(board)
+    state.append(c_s)
+    
+    play(board)
 
-# Checks whether the player has three 
-# of their marks in a horizontal row 
-def row_win(board, player): 
-	for x in range(len(board)): 
-		win = True
-		
-		for y in range(len(board)): 
-			if board[x, y] != player: 
-				win = False
-				continue
-				
-		if win == True: 
-			return(win) 
-	return(win) 
+board = np.array(np.zeros([3,3], dtype='int'))
+play(board)
 
-# Checks whether the player has three 
-# of their marks in a vertical row 
-def col_win(board, player): 
-	for x in range(len(board)): 
-		win = True
-		
-		for y in range(len(board)): 
-			if board[y][x] != player: 
-				win = False
-				continue
-				
-		if win == True: 
-			return(win) 
-	return(win) 
-
-# Checks whether the player has three 
-# of their marks in a diagonal row 
-def diag_win(board, player): 
-	win = True
-	
-	for x in range(len(board)): 
-		if board[x, x] != player: 
-			win = False
-	return(win) 
-
-# Evaluates whether there is 
-# a winner or a tie 
-def evaluate(board): 
-	winner = 0
-	
-	for player in [1, 2]: 
-		if (row_win(board, player) or
-			col_win(board,player) or
-			diag_win(board,player)): 
-				
-			winner = player 
-			
-	if np.all(board != 0) and winner == 0: 
-		winner = -1
-	return winner 
-
-# Main function to start the game 
-def play_game(): 
-	board, winner, counter, tag = create_board(), 0, 1, 1
-	print(board) 
-	sleep(1) 
-	
-	while winner == 0: 
-		for player in [1, 2]: 
-			board = random_place(board, tag) 
-			print("Board after " + str(counter) + " move") 
-			print(board) 
-			sleep(2) 
-			counter += 1
-			winner = evaluate(board) 
-			if winner != 0: 
-				break
-	return(winner) 
-
-# Driver Code 
-print("Winner is: " + str(play_game())) 
