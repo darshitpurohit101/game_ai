07581d410c6ef723b9367fd9d6a14622964130b8
darshitpurohit101 <purohitdarshit101@gmail.com>
2020-12-24 18:22:28 +0530

    new class

--- tic_tae_toe.py
+++ tic_tae_toe.py
@@ -1,47 +1,52 @@
 import numpy as np
 import random
 
-def check_board(board):
-    empty = []
+class tic_tac_toe():
     
-    for row in range(len(board)):
-        for box in range(len(board[row])):
-            if board[row][box] == 0:
-                empty.append([row,box])
-    if len(empty) == 0:
-        return 0
-    else:
-        return empty
-
-def state_reg(board):
-    current_state = []
-    for x in board:
-        for j in range(len(x)):
-            current_state.append(x[j])
-    return current_state
+    def __init__(self):
+        self.state = []
     
-def play(board):
-    state = []
-    #Player one
-    x,y = map(int,input("Human's turn! ").split(' '))
-    board[(x,y)] = 1
+    def check_board(self, sboard):
+        empty = []
+        
+        for row in range(len(board)):
+            for box in range(len(board[row])):
+                if board[row][box] == 0:
+                    empty.append([row,box])
+        if len(empty) == 0:
+            return 0
+        else:
+            return empty
     
-    c_s = state_reg(board)
-    state.append(c_s)
-
-    possible_play = check_board(board)
-    if possible_play == 0:
-        print(state)
-        return 0
-    #player two
-    selection = random.choice(possible_play)
-    board[selection[0]][selection[1]] = 2
+    def state_reg(self,board):
+        current_state = []
+        for x in board:
+            for j in range(len(x)):
+                current_state.append(x[j])
+        return current_state
+        
+    def play(self, board):
+        #Player one
+        x,y = map(int,input("Human's turn! ").split(' '))
+        board[(x,y)] = 1
+        
+        c_s = self.state_reg(board)
+        self.state.append(c_s)
     
-    c_s = state_reg(board)
-    state.append(c_s)
-    
-    play(board)
+        possible_play = self.check_board(board)
+        if possible_play == 0:
+            print(self.state)
+            return 0
+        #player two
+        selection = random.choice(possible_play)
+        board[selection[0]][selection[1]] = 2
+        
+        c_s = self.state_reg(board)
+        self.state.append(c_s)
+        
+        self.play(board)
 
 board = np.array(np.zeros([3,3], dtype='int'))
-play(board)
+c = tic_tac_toe()
+c.play(board)
 
